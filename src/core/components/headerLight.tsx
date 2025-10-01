f386148e84bb3b763beefea5baff4a80f88303b2
And5reas <And5reas.porcel@hotmail.com>
2025-10-01 16:25:32 -0300

    feat: creating login header

---
+++ src/core/components/headerLight.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react'
+import { FiSearch, FiMenu, FiX } from 'react-icons/fi'
+
+const HeaderLight = () => {
+  return <header className="absolute left-0 top-0 h-20 w-full bg-[#0044A9]"></header>
+}
+
+export default HeaderLight
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Login from './core/views/Login'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold text-gray-800">Hello World</h1>
-        <p className="text-lg text-gray-600">
-          Projeto MVVM React + Vite + TypeScript + TailwindCSS
-        </p>
+        <Login />
       </div>
     </div>
   </React.StrictMode>
