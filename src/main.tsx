b9ca94e58683df6731e4db12dc15841d557f3713
Luan Calasans <luancalasans@unisanta.br>
2025-10-01 16:21:56 -0300

    style: configura tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="flex min-h-screen items-center justify-center bg-background-secondary">
       <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold text-gray-800">Hello World</h1>
-        <p className="text-lg text-gray-600">
+        <h1 className="mb-4 text-huge font-semibold text-text-primary">Hello World</h1>
+        <p className="text-base font-normal text-text-secondary">
           Projeto MVVM React + Vite + TypeScript + TailwindCSS
         </p>
       </div>
