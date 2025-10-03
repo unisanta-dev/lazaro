8902a8c7fd2fdeab45c3f73dec1323b0790b2637
Luan Calasans <luancalasans@unisanta.br>
2025-10-03 08:59:33 -0300

    refactor: remove codigo desnecessário da main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div className="bg-background-secondary flex min-h-screen items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-huge text-text-primary mb-4 font-semibold">Hello World</h1>
-        <p className="text-text-secondary text-base font-normal">
-          Projeto MVVM React + Vite + TypeScript + TailwindCSS
-        </p>
-      </div>
-    </div>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode></React.StrictMode>)
